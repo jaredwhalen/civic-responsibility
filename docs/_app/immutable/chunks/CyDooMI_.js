1df695a805f11d4979c472c7f03e628cc79d835e
Jared Whalen <jared.m.whalen@gmail.com>
2025-08-20 14:30:03 -0400

    update github pages

--- docs/_app/immutable/chunks/CyDooMI_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ihaofo?.base??"",a=globalThis.__sveltekit_ihaofo?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/DXclTtgI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1kujoop?.base??"",a=globalThis.__sveltekit_1kujoop?.assets??s;export{a,s as b};
