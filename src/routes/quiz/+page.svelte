7b4f44b458303e95f6fac93502258897c3670b7f
Jared Whalen <jared.m.whalen@gmail.com>
2025-10-27 17:18:57 -0400

    Add email signup to quiz

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Quiz from '$lib/components/quiz/Quiz.svelte';
+	import EmailSignupSection from '$lib/components/EmailSignupSection.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Quiz />
-
+<EmailSignupSection />
 <Footer />
