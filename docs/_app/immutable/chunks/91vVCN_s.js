35d0d878b423bc133bf2917e073a03ed1d9eddd5
Jared Whalen <jared.m.whalen@gmail.com>
2025-10-31 11:08:34 -0400

    update github pages

--- docs/_app/immutable/chunks/91vVCN_s.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_yrapbl?.base??"",a=globalThis.__sveltekit_yrapbl?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/DQ3BpWB-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_tjbu2n?.base??"",t=globalThis.__sveltekit_tjbu2n?.assets??s;export{t as a,s as b};
